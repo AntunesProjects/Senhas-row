c9a02ea33200e05ca2a85a05f9242ae37c4e3916
AntunesProjects <antunesmatheus.contato@gmail.com>
2024-01-15 17:24:38 -0300

    audio

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,21 @@
-const socket = new WebSocket('ws://54.207.89.214:8080'); // Conectar ao servidor WebSocket
+const socket = new WebSocket('ws://127.0.0.1:8080'); // Conectar ao servidor WebSocket
+const audioElement = new Audio('/assets/bell.mp3');
 
 // Função para atualizar a interface com os dados recebidos do servidor
 function updateUI(data) {
-  document.querySelector('.numero').textContent = data.senhaAtual;
+  const numeroElement = document.querySelector('.numero');
   const historicoElements = document.querySelectorAll('.senha p');
+
+  // Verifica se o número foi alterado
+  if (numeroElement.textContent !== data.senhaAtual.toString()) {
+    // Atualiza o número na interface
+    numeroElement.textContent = data.senhaAtual;
+
+    // Reproduz o áudio sempre que o número é alterado
+    audioElement.play();
+  }
+
+  // Atualiza o histórico na interface
   for (let i = 0; i < data.historico.length; i++) {
     historicoElements[i].textContent = data.historico[i];
   }
@@ -14,3 +26,13 @@ socket.addEventListener('message', (event) => {
   const data = JSON.parse(event.data);
   updateUI(data);
 });
+
+// Utiliza MutationObserver para observar mudanças no conteúdo do elemento que exibe o número
+const numeroElement = document.querySelector('.numero');
+const observer = new MutationObserver(() => {
+  // Reproduz o áudio sempre que o número é alterado
+  audioElement.play();
+});
+
+// Configura o MutationObserver para observar mudanças no conteúdo do elemento
+observer.observe(numeroElement, { childList: true, subtree: true });
